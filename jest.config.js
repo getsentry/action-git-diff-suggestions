526863e1b800a6b019646c566aa97b588dcbe462
Billy Vong <billyvg@users.noreply.github.com>
2020-11-23 16:41:54 -0800

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  clearMocks: true,
+  moduleFileExtensions: ['js', 'ts'],
+  moduleNameMapper: {
+    '^@app/(.*)$': '<rootDir>/src/$1',
+    '^@test/(.*)$': '<rootDir>/test/$1',
+  },
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  testRunner: 'jest-circus/runner',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  verbose: true,
+};
